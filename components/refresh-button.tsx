a075781cf731319e201a59fed4e24c88c08369d0
Arav Dave <aravdave@gmail.com>
2024-01-14 15:22:22 -0500

    Commented out starter code to avoid deployment issues

--- components/refresh-button.tsx
+++ components/refresh-button.tsx
@@ -1,25 +1,25 @@
-'use client'
+// 'use client'
 
-import { useRouter } from 'next/navigation'
-import { useTransition } from 'react'
+// import { useRouter } from 'next/navigation'
+// import { useTransition } from 'react'
 
-export default function RefreshButton() {
-  const router = useRouter()
-  const [isPending, startTransition] = useTransition()
+// export default function RefreshButton() {
+//   const router = useRouter()
+//   const [isPending, startTransition] = useTransition()
 
-  return (
-    <button
-      className={`${
-        isPending ? 'cursor-not-allowed text-gray-400' : ''
-      } text-sm text-gray-500 hover:text-gray-900`}
-      disabled={isPending}
-      onClick={() => {
-        startTransition(() => {
-          router.refresh()
-        })
-      }}
-    >
-      {isPending ? 'Refreshing...' : 'Refresh'}
-    </button>
-  )
-}
+//   return (
+//     <button
+//       className={`${
+//         isPending ? 'cursor-not-allowed text-gray-400' : ''
+//       } text-sm text-gray-500 hover:text-gray-900`}
+//       disabled={isPending}
+//       onClick={() => {
+//         startTransition(() => {
+//           router.refresh()
+//         })
+//       }}
+//     >
+//       {isPending ? 'Refreshing...' : 'Refresh'}
+//     </button>
+//   )
+// }
